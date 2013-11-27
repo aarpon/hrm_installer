02d48f625c405a52272f9f41756ca70da2db9d3b
Niko Ehrenfeuchter <mail@he1ix.org>
2013-11-27 14:28:36 +0100

    Improve readability.

--- conf-hrm.sh
+++ conf-hrm.sh
@@ -10,14 +10,15 @@ sedconf /etc/hrm.conf "HRM_HOME=\"/path/to/hrm/home\"" "HRM_HOME=\"$hrmdir\""
 sedconf /etc/hrm.conf "SUSER=\"hrm\"" "SUSER=\"hrm-user\""
 
 # copy more config files
-cp $hrmdir/config/samples/hrm_server_config.inc.sample $hrmdir/config/hrm_server_config.inc
+CONF_SRV="$hrmdir/config/hrm_server_config.inc"
+cp $hrmdir/config/samples/hrm_server_config.inc.sample $CONF_SRV
 
 # do substitutions in config file
-sedconf $hrmdir/config/hrm_server_config.inc '$db_type = "mysql";' '$db_type = "'$dbtype'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$db_user = "dbuser";' '$db_user = "'$db_user'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$db_password = "dbpasswd";' '$db_password = "'$db_pass'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$hrm_path = "/var/www/html/hrm";' '$hrm_path = "'$hrmdir'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$local_huygens_core = "/usr/local/bin/hucore";' '$local_huygens_core = "'$hucorepath'";'
+sedconf $CONF_SRV '$db_type = "mysql";' '$db_type = "'$dbtype'";'
+sedconf $CONF_SRV '$db_user = "dbuser";' '$db_user = "'$db_user'";'
+sedconf $CONF_SRV '$db_password = "dbpasswd";' '$db_password = "'$db_pass'";'
+sedconf $CONF_SRV '$hrm_path = "/var/www/html/hrm";' '$hrm_path = "'$hrmdir'";'
+sedconf $CONF_SRV '$local_huygens_core = "/usr/local/bin/hucore";' '$local_huygens_core = "'$hucorepath'";'
 
 # get image store
 echo "Enter image storage directory"
@@ -37,13 +38,13 @@ then
 fi
 
 sedconf /etc/hrm.conf "HRM_DATA=\"/path/to/hrm/data\"" "HRM_DATA=\"$imgdir\""
-sedconf $hrmdir/config/hrm_server_config.inc '$image_folder = "/path/to/hrm_data";' '$image_folder = "'$imgdir'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$huygens_server_image_folder = "/path/to/hrm_data/";' '$huygens_server_image_folder = "'$imgdir'/";'
+sedconf $CONF_SRV '$image_folder = "/path/to/hrm_data";' '$image_folder = "'$imgdir'";'
+sedconf $CONF_SRV '$huygens_server_image_folder = "/path/to/hrm_data/";' '$huygens_server_image_folder = "'$imgdir'/";'
 
 echo "Enter HRM administrator's email address"
 hrmemail=`readstring`
-sedconf $hrmdir/config/hrm_server_config.inc '$email_sender = "hrm@localhost";' '$email_sender = "'$hrmemail'";'
-sedconf $hrmdir/config/hrm_server_config.inc '$email_admin = "hrm@localhost";' '$email_admin = "'$hrmemail'";'
+sedconf $CONF_SRV '$email_sender = "hrm@localhost";' '$email_sender = "'$hrmemail'";'
+sedconf $CONF_SRV '$email_admin = "hrm@localhost";' '$email_admin = "'$hrmemail'";'
 
 # assume client and server run on same machine, config file are identical
-ln -s $hrmdir/config/hrm_server_config.inc $hrmdir/config/hrm_client_config.inc
+ln -s $CONF_SRV $hrmdir/config/hrm_client_config.inc
