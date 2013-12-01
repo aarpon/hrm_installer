aarpon/hrm_installer

commit 91c472dfc7e96cad57f5f9be72d858049a0c0ca4

Allow to configure a DB name.
refs #97

conf-db-generic.sh
@@ -4,7 +4,6 @@ source funs.sh
 
 echo "Create new database account for HRM? [y/n]"
 if [ $(readkey) == "y" ] ; then
-    # get user credentials
     echo "Enter new user name to create for the HRM database"
     db_user=`readstring "hrm_user"`
 
@@ -18,10 +17,14 @@ if [ $(readkey) == "y" ] ; then
         dbtype="postgres"
     fi
 else
-    # get user credentials
     echo "Enter existing user name for the HRM database"
     db_user=`readstring "hrm_user"`
 
     echo "Enter password for the HRM database user"
     db_pass=`readstring`
 fi
+
+# The dbupdate script takes care if the DB already exists or creates it
+# otherwise, so wee need the name in any case for the config file:
+echo "Enter the name of the database to use for the HRM"
+db_name=`readstring "hrm"`

conf-hrm.sh
@@ -17,6 +17,7 @@ cp $hrmdir/config/samples/hrm_server_config.inc.sample $CONF_SRV
 sedconf $CONF_SRV '$db_type = "mysql";' '$db_type = "'$dbtype'";'
 sedconf $CONF_SRV '$db_user = "dbuser";' '$db_user = "'$db_user'";'
 sedconf $CONF_SRV '$db_password = "dbpasswd";' '$db_password = "'$db_pass'";'
+sedconf $CONF_SRV '$db_name = "hrm";' '$db_name = "'$db_name'";'
 sedconf $CONF_SRV '$hrm_path = "/var/www/html/hrm";' '$hrm_path = "'$hrmdir'";'
 sedconf $CONF_SRV '$local_huygens_core = "/usr/local/bin/hucore";' '$local_huygens_core = "'$hucorepath'";'
 

commit d269fa5554d1a197a8d1c8b97f7801fbd5d663b9

DB name must be known before sourcing DB-specific files.
Bug introduced in commit:91c472dfc7e96cad57f5f9be72d858049a0c0ca4
refs #97

conf-db-generic.sh
@@ -2,6 +2,11 @@
 
 source funs.sh
 
+# The dbupdate script takes care if the DB already exists or creates it
+# otherwise, so wee need the name in any case for the config file:
+echo "Enter the name of the database to use for the HRM"
+db_name=`readstring "hrm"`
+
 echo "Create new database account for HRM?"
 if [ $(readkey_choice) == "y" ] ; then
     echo -e "\nEnter new user name to create for the HRM database"
@@ -21,8 +26,3 @@ else
     echo "Enter password for the HRM database user"
     db_pass=`readstring`
 fi
-
-# The dbupdate script takes care if the DB already exists or creates it
-# otherwise, so wee need the name in any case for the config file:
-echo "Enter the name of the database to use for the HRM"
-db_name=`readstring "hrm"`