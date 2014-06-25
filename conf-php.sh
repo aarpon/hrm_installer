08dbe7d6722c7a1ca8e5154e5868fabaade4b4e9
user <user@localhost.localdomain>
2014-06-25 13:00:43 +0200

    corrected date.timezone parsing in php.ini

--- conf-php.sh
+++ conf-php.sh
@@ -8,6 +8,8 @@ postmax=`readstring "256M"`
 echo "Enter PHP upload_max_filesize (limits file size for browser uploads)"
 upmax=`readstring "256M"`
 
+dist="Fedora"
+
 if [ "$dist" == "Ubuntu" ]
 then
 	phpinipath="/etc/php5/apache2/php.ini"
@@ -15,7 +17,7 @@ elif [ "$dist" == "Fedora" ]
 then
 	phpinipath="/etc/php.ini"
 	tz=`ls -l /etc/localtime | sed "s:zoneinfo/:\n:g" | tail -1`
-	sedconf $phpinipath ";date.timezone = .*" "date.timezone = $tz"
+	sedconf $phpinipath "\;date\.timezone =" "date.timezone = $tz"
 else
 	abort "Distribution unsupported."
 fi
