9e8e3e05bb2cffeb6872faad9487611df9b32229
Egor Zindy <ezindy@gmail.com>
2018-12-05 19:48:05 +0000

    retabbed and commented out systemd hrmd startup

--- scripts/conf-qm.sh
+++ scripts/conf-qm.sh
@@ -9,36 +9,36 @@ then
     ans="v"
 elif [[ "$dist" == "Ubuntu" ]]
 then
-	if [[ "$vers" < "14.10" ]]
-	then
-		ans="v"
-	else
-		ans="d"
-	fi
+    if [[ "$vers" < "14.10" ]]
+    then
+        ans="v"
+    else
+        ans="d"
+    fi
 elif [[ "$dist" == "Fedora" ]]
 then
     ans="d"
 else
-	echo "Please select which init system your distribution is using, 'systemd' [d]"
-	echo "or classical 'System-V-init' [v] (shellscripts in /etc/init.d/ or similar)"
-	ans=`readkey_choice 'd' 'v'`
+    echo "Please select which init system your distribution is using, 'systemd' [d]"
+    echo "or classical 'System-V-init' [v] (shellscripts in /etc/init.d/ or similar)"
+    ans=`readkey_choice 'd' 'v'`
 fi
 
 case $ans in
-	d)
-		inittype="systemd"
-		;;
-	v)
-		inittype="sysv"
-		;;
-	*) abort "Wrong init system type selected!"
-		;;
+    d)
+        inittype="systemd"
+        ;;
+    v)
+        inittype="sysv"
+        ;;
+    *) abort "Wrong init system type selected!"
+        ;;
 esac
 echo "Configuring startup for init system type '$inittype'."
 
 
 if [ "$inittype" == "systemd" ] ; then
-    if [ "$dbtype" == "mysql" ]; then
+    if [ "$dbtype" == "pgsql" ]; then
         sedconf $hrmdir/resources/systemd/hrmd.service "Requires=mysql" "Requires=postgresql"
         sedconf $hrmdir/resources/systemd/hrmd.service "After=mysql" "After=postgresql"
     fi
@@ -46,24 +46,29 @@ if [ "$inittype" == "systemd" ] ; then
     sedconf $hrmdir/resources/systemd/hrmd.service "Group=hrm" "Group=$sysgroup"
     sedconf $hrmdir/resources/systemd/hrmd.service "ExecStart=/var/www/html/hrm" "ExecStart=$hrmdir"
 
-	cp $hrmdir/resources/systemd/hrmd.service /etc/systemd/system/
-	#chmod +x /etc/systemd/system/hrmd.service
-	systemctl daemon-reload
-	systemctl enable hrmd.service
-	systemctl start hrmd.service
-	systemctl status hrmd.service
+    cp $hrmdir/resources/systemd/hrmd.service /etc/systemd/system/
+    #chmod +x /etc/systemd/system/hrmd.service
+    systemctl daemon-reload
+    systemctl enable hrmd.service
+    #systemctl start hrmd.service
+    #systemctl status hrmd.service
+
+    if [ "$dist" == "Fedora" ] ; then
+        chkconfig httpd on
+    fi
+
 elif [ "$inittype" == "sysv" ] ; then
-	cp $hrmdir/resources/sysv-init-lsb/hrmd /etc/init.d/
-	chmod +x /etc/init.d/hrmd
-	if [ "$dist" == "Ubuntu" ] || [[ "$dist" == "Debian" ]]; then
-		update-rc.d hrmd defaults
-	elif [ "$dist" == "Fedora" ] ; then
-		chkconfig hrmd on
-	else
-		abort "Distribution unsupported."
-	fi
-	service hrmd start > /dev/null
-	sleep 2
+    cp $hrmdir/resources/sysv-init-lsb/hrmd /etc/init.d/
+    chmod +x /etc/init.d/hrmd
+    if [ "$dist" == "Ubuntu" ] || [[ "$dist" == "Debian" ]]; then
+        update-rc.d hrmd defaults
+    elif [ "$dist" == "Fedora" ] ; then
+        chkconfig hrmd on
+    else
+        abort "Distribution unsupported."
+    fi
+    service hrmd start > /dev/null
+    sleep 2
     /etc/init.d/hrmd status
 fi
 
