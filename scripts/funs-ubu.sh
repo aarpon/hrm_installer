93607cb20d6d6b44aa4bbeaadb0d347340de1fe7
zindy <ezindy@gmail.com>
2022-01-17 10:00:56 +0000

    fix for detecting if mariadb-server is installed

--- scripts/funs-ubu.sh
+++ scripts/funs-ubu.sh
@@ -8,10 +8,25 @@ function packages_missing() {
     # Check the debian dpkg database for one or more packages to ensure they
     # are installed on the system. Returns the name of all packages that don't
     # have the dpkg-query status "install ok installed".
-    local VALID='install ok installed$'
+    #
+    # TODO Please someone confirm the followin fix! (@zindy to blame).
+    # There was an issue detecting the "mariadb-server" package, so I
+    # extented the search by adding a star at the end of the package name
+    # so we can detect that "mariadb-server-10.1" (for example) is installed.
+    # However, $VALID may not be at the beginning of the status returned by
+    # dpkg-query. For example, the search for "mariadb-server*" returns:
+    #
+    # unknown ok not-installedinstall ok installedunknown ok \
+    # not-installedunknown ok not-installedinstall ok
+    #
+    # Grep-ing for 'install ok installed' somewhere in the $Status string
+    # should be enough to confirm a package is installed, but do let me know
+    # if this change breaks the query test for any other packages used by HRM.
+
+    local VALID='install ok installed'
     unset PKGSMISSING
     for PKG in $* ; do
-        if ! dpkg-query -W -f='${Status}' $PKG 2>&1 | grep -q "$VALID" ; then
+        if ! dpkg-query -W -f='${Status}' "$PKG*" 2>&1 | grep -q "$VALID" ; then
             PKGSMISSING="$PKGSMISSING $PKG"
         fi
     done
